4f652ba13ef013bc919cb42e8da19847b52bc333
Bella Evalle <bellaevalle@yahoo.com>
2020-08-12 14:05:03 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 10, 'M', 'dark brown', 'intense', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'dark brown', 'nice', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 19, 'M', 'green', 'slimy', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 13, 'F', 'dark brown', 'goofy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'black', 'grinchy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 6, 'F', 'blue', 'naive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 20, 'M', 'black', 'aggresive', 0);
