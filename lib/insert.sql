f574f98629bb65311d0b6c2e4dea3eb4d6740b16
emjose <emjose@gmail.com>
2020-08-11 23:31:18 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- CREATE Table bears (
+--     id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     age INTEGER,
+--     gender TEXT,
+--     color TEXT,
+--     temperament TEXT,
+--     alive BOOLEAN
+-- );
+-- ID - NAME - AGE - GENDER - COLOR - TEMPERAMENT - ALIVE(BOOLEAN)
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 10, "M", "red", "grumpy", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 11, "M", "orange", "dopey", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 12, "F", "yellow", "goofy", 1);
+INSERT INTO bears VALUES (4, "Sargeant Brown", 13, "M", "green", "happy", 0);
+INSERT INTO bears VALUES (5, "Melissa", 14, "F", "blue", "bashful", 1);
+INSERT INTO bears VALUES (6, "Grinch", 15, "M", "indigo", "sneezy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 16, "F", "violet", "sleepy", 1);
+INSERT INTO bears VALUES (8, null, 17, "M", "black", "mean", 0);
