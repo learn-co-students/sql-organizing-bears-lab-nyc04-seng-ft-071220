76252a3ac11b513c42b96d3542daa11c41ceaeeb
wilsonng21 <wilsongch92@gmail.com>
2020-08-12 09:26:32 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears
+VALUES 
+(1, 'Mr.Chocolate', 8, 'M', 'brown', 'shy', TRUE),
+(2, 'Rowdy', 12, 'M', 'brown', 'playful', TRUE),
+(3, 'Tabitha', 15, 'F', 'black', 'outgoing', TRUE),
+(4, 'Sergeant Brown', 18, 'M', 'brown', 'mean', FALSE),
+(5, 'Melissa', 5, 'F', 'brown', 'joyful', TRUE),
+(6, 'Grinch', 20, 'M', 'black', 'quiet', TRUE),
+(7, 'Wendy', 3, 'F', 'black', 'adventurous', TRUE),
+(8, NULL, 28, 'M', 'brown', 'murderous', FALSE);
+    
