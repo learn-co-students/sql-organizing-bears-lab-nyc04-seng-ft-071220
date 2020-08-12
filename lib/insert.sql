2d3eff5459f212fef20ce40dae2c558bd5ac559b
Dong Xia <dong7777xia@gmail.com>
2020-08-12 14:08:22 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-VALUES ("Mr.Chocolate", 12, "M", "Black", "Nice", TRUE),
-("Rowdy", 13, "M", "Black", "Nice", TRUE),
-("Tabitha", 4, "F", "Brown", "Nice", TRUE),
-("Sergeant Brown", 20, "M", "Brown", "Mean", TRUE),
-("Melissa", 20, "F", "Brown", "Nice", TRUE),
-("Grinch", 22, "F", "Black", "Mean", TRUE),
-("Wendy", 15, "F", "Brown", "Nice", TRUE),
-(NULL, 10, "M", "White", "Nice", FALSE);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
