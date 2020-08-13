e087cac5350a5ec30afff15591be333cb62899c5
iannakim <dear.hyunji@gmail.com>
2020-08-12 23:16:00 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,10 @@
 require_relative 'environment'
+require 'pry'
 
 db = SQLite3::Database.new('../db/lab_database.db')
+
+binding.pry
+
 sql_runner = SQLRunner.new(db)
 
 sql_runner.execute_sql_file
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1, "Mr.Chocolate", 3, "M", "Brown", "Gentle", True),
+(2, "Rowdy", 1, "M", "Brown", "Social", True),
+(3, "Tabitha", 4, "F", "Brown", "Aggressive", False),
+(4, "Sergeant Brown", 2, "M", "Brown", "Gentle", True),
+(5, "Melissa", 2, "F", "Brown", "Aggressive", True),
+(6, "Grinch", 3, "M", "Brown", "Gentle", True),
+(7, "Wendy", 5, "F", "Brown", "Playful", False),
+(8, NULL, 1, "F", "Brown", "Gentle", True)
